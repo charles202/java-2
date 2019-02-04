73ad9a695bce6be5862b4e0eaa7a5990dd8931f1
Francisco <francisco.olmos2@mavs.uta.edu>
2019-02-03 22:24:35 -0600

    errors

--- Game.java
+++ Game.java
@@ -1,38 +1,35 @@
-//Charles Bejarano 
+//Charles Bejarano
 
-public class Game extends Rental 
-{ 
+public class Game extends Rental
+{
 	public enum GameType{XBOX,PLAYSTATION,NINTENDO};
-	private GameType gType = GameType.XBOX; 
-	
+	private GameType gType = GameType.XBOX;
+
 	public Game()
 	{
 		super();
+		//set the default game type
 	}
-	
+
 	public Game(String title, String rentalCode, double price, GameType type)
 	{
 		super(title, rentalCode, price);
+		//missing set method 
 	}
-	
+
 	public void setGameType(GameType type)
 	{
 		this.gType = gType;
 	}
-	
+
 	public GameType getGameType()
 	{
 		return gType;
 	}
-	
+
 	public String toString()
 	{
 		return(super.toString() + "Game Type" + gType);
 	}
-	
+
 }
-		
-	
-		
-		
-		
